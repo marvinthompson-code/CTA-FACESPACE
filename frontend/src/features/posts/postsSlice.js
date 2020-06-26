4ad19b1b7c4ec4c62ceb6a5e66b16c4f9bee5113
Marvin <marvinthompson@pursuit.org>
2020-06-26 18:23:52 -0400

    working on share and delete functionality

--- frontend/src/features/posts/postsSlice.js
+++ frontend/src/features/posts/postsSlice.js
@@ -7,7 +7,13 @@ export const postsSlice = createSlice({
     initialState: [],
     reducers: {
         addPost: (state, action) => { state.unshift(action.payload) },
-        recieveAllPosts:  (state, action) => action.payload    
+        recieveAllPosts:  (state, action) => action.payload,
+        sharePost: (state, action) => action.payload,
+        deletePost: (state, action) => {
+            state.filter(post => {
+                return post.id !== action.payload
+            })
+        } 
     }
 })
 
@@ -23,6 +29,18 @@ export const createNewPost = (post) => async (dispatch, getState) => {
     })
     let { newPost } = res.data.body
     dispatch(addPost(newPost))
+}
+
+export const deletePostAsync = (id) => async (dispatch) => {
+        await axios({
+        method: "delete",
+        url: `${apiURL()}/posts/${id}`
+    })
+    dispatch(deletePost(id))
+}
+
+export const sharePostAsync = (post) => async (dispatch, getState) => {
+    
 }
 
 export const selectPosts = (state) => state.posts
